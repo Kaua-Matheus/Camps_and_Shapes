fd33c1e4829d9944fda3f326e710856e2726e494
Kauã Matheus <kauamatheussilva06@gmail.com>
2026-04-06 20:24:33 -0300

    fix: Interface corrections and entities

--- scripts/enemies/golem.gd
+++ scripts/enemies/golem.gd
@@ -28,7 +28,7 @@ var direction = 1
 var can_throw = true
 
 func _ready() -> void:
-	go_to_walk_state()
+	go_to_idle_state()
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -37,12 +37,12 @@ func _physics_process(delta: float) -> void:
 	match status:
 		SkeletonState.idle:
 			idle_state(delta)
-		SkeletonState.walk:
-			walk_state(delta)
-		SkeletonState.attack:
-			attack_state(delta)
-		SkeletonState.dead:
-			dead_state(delta)
+		#SkeletonState.walk:
+			#walk_state(delta)
+		#SkeletonState.attack:
+			#attack_state(delta)
+		#SkeletonState.dead:
+			#dead_state(delta)
 
 	move_and_slide()
 	
@@ -51,62 +51,62 @@ func go_to_idle_state():
 	status = SkeletonState.idle
 	animation.play("idle")
 	
-func go_to_walk_state():
-	status = SkeletonState.walk
-	animation.play("walk")
-	
-func go_to_attack_state():
-	status = SkeletonState.attack
-	animation.play("attack")
-	velocity = Vector2.ZERO
-	can_throw = true
-	
-func go_to_dead_state():
-	status = SkeletonState.dead
-	animation.play("dead")
-	velocity = Vector2.ZERO
-	hitbox.process_mode = Node.PROCESS_MODE_DISABLED
+#func go_to_walk_state():
+	#status = SkeletonState.walk
+	#animation.play("walk")
+	#
+#func go_to_attack_state():
+	#status = SkeletonState.attack
+	#animation.play("attack")
+	#velocity = Vector2.ZERO
+	#can_throw = true
+	#
+#func go_to_dead_state():
+	#status = SkeletonState.dead
+	#animation.play("dead")
+	#velocity = Vector2.ZERO
+	#hitbox.process_mode = Node.PROCESS_MODE_DISABLED
 	
 	
 func idle_state(_delta):
 	pass
 	
-func walk_state(_delta):
-	if animation.frame == 3 or animation.frame == 4:
-		velocity.x = SPEED * direction
-	else:
-		velocity.x = 0
-	
-	if wall_detector.is_colliding():
-		scale.x *= -1
-		direction *= -1
-		
-	if not ground_detector.is_colliding():
-		scale.x *= -1
-		direction *= -1
-		
-	if player_detector.is_colliding():
-		go_to_attack_state()
-		return
-	
-func dead_state(_delta):
-	pass
-
-func attack_state(_delta):
-	if animation.frame == 2 and can_throw:
-		throw_bone()
-		can_throw = false
-
-func take_damage():
-	go_to_dead_state()
-
-func throw_bone():
-	var new_bone = ROCK.instantiate()
-	add_sibling(new_bone)
-	new_bone.position = bone_start_position.global_position
-	new_bone.set_direction(self.direction)
-
-func _on_animated_sprite_2d_animation_finished() -> void:
-	if animation.animation == "attack":
-		go_to_walk_state()
-		return
+#func walk_state(_delta):
+	#if animation.frame == 3 or animation.frame == 4:
+		#velocity.x = SPEED * direction
+	#else:
+		#velocity.x = 0
+	#
+	#if wall_detector.is_colliding():
+		#scale.x *= -1
+		#direction *= -1
+		#
+	#if not ground_detector.is_colliding():
+		#scale.x *= -1
+		#direction *= -1
+		#
+	#if player_detector.is_colliding():
+		#go_to_attack_state()
+		#return
+	#
+#func dead_state(_delta):
+	#pass
+#
+#func attack_state(_delta):
+	#if animation.frame == 2 and can_throw:
+		#throw_bone()
+		#can_throw = false
+#
+#func take_damage():
+	#go_to_dead_state()
+#
+#func throw_bone():
+	#var new_bone = ROCK.instantiate()
+	#add_sibling(new_bone)
+	#new_bone.position = bone_start_position.global_position
+	#new_bone.set_direction(self.direction)
+#
+#func _on_animated_sprite_2d_animation_finished() -> void:
+	#if animation.animation == "attack":
+		#go_to_walk_state()
+		#return
