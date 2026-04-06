efcda1cde6b6a0ae2e754141432a2dee40975329
Kauã Matheus <kauamatheussilva06@gmail.com>
2026-04-06 20:55:11 -0300

    feat: Added walk in golem

--- scripts/enemies/golem.gd
+++ scripts/enemies/golem.gd
@@ -10,59 +10,59 @@ extends CharacterBody2D
 
 # Bone
 const ROCK = preload("uid://b6tgl8mkre3l2")
-@onready var bone_start_position: Node2D = $BoneStartPosition
+@onready var rock_start_position: Node2D = $RockStartPosition
 
-enum SkeletonState {
+enum GolemState {
 	idle,
 	walk,
 	attack,
-	dead
+	#dead
 }
 
 const SPEED = 20.0
 const JUMP_VELOCITY = -250.0
 
-var status: SkeletonState
+var status: GolemState
 
 var direction = 1
 var can_throw = true
 
 func _ready() -> void:
-	go_to_idle_state()
+	go_to_walk_state()
 
 func _physics_process(delta: float) -> void:
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	#if not is_on_floor():
+		#velocity += get_gravity() * delta
 		
 	match status:
-		SkeletonState.idle:
+		GolemState.idle:
 			idle_state(delta)
-		#SkeletonState.walk:
-			#walk_state(delta)
-		#SkeletonState.attack:
+		GolemState.walk:
+			walk_state(delta)
+		#GolemState.attack:
 			#attack_state(delta)
-		#SkeletonState.dead:
+		#GolemState.dead:
 			#dead_state(delta)
 
 	move_and_slide()
 	
 	
 func go_to_idle_state():
-	status = SkeletonState.idle
+	status = GolemState.idle
 	animation.play("idle")
 	
-#func go_to_walk_state():
-	#status = SkeletonState.walk
-	#animation.play("walk")
-	#
-#func go_to_attack_state():
-	#status = SkeletonState.attack
+func go_to_walk_state():
+	status = GolemState.walk
+	animation.play("walk")
+	
+func go_to_attack_state():
+	status = GolemState.attack
 	#animation.play("attack")
-	#velocity = Vector2.ZERO
-	#can_throw = true
-	#
+	velocity = Vector2.ZERO
+	can_throw = true
+	
 #func go_to_dead_state():
-	#status = SkeletonState.dead
+	#status = GolemState.dead
 	#animation.play("dead")
 	#velocity = Vector2.ZERO
 	#hitbox.process_mode = Node.PROCESS_MODE_DISABLED
@@ -71,40 +71,36 @@ func go_to_idle_state():
 func idle_state(_delta):
 	pass
 	
-#func walk_state(_delta):
-	#if animation.frame == 3 or animation.frame == 4:
-		#velocity.x = SPEED * direction
-	#else:
-		#velocity.x = 0
-	#
-	#if wall_detector.is_colliding():
-		#scale.x *= -1
-		#direction *= -1
-		#
+func walk_state(_delta):
+	velocity.x = SPEED * direction
+	
+	if wall_detector.is_colliding():
+		scale.x *= -1
+		direction *= -1
+		
 	#if not ground_detector.is_colliding():
 		#scale.x *= -1
 		#direction *= -1
-		#
-	#if player_detector.is_colliding():
-		#go_to_attack_state()
-		#return
-	#
+		
+	if player_detector.is_colliding():
+		go_to_attack_state()
+		return
+	
 #func dead_state(_delta):
 	#pass
 #
-#func attack_state(_delta):
-	#if animation.frame == 2 and can_throw:
-		#throw_bone()
-		#can_throw = false
+func attack_state(_delta):
+	throw_rock()
+	can_throw = false
 #
 #func take_damage():
 	#go_to_dead_state()
 #
-#func throw_bone():
-	#var new_bone = ROCK.instantiate()
-	#add_sibling(new_bone)
-	#new_bone.position = bone_start_position.global_position
-	#new_bone.set_direction(self.direction)
+func throw_rock():
+	var new_rock = ROCK.instantiate()
+	add_sibling(new_rock)
+	new_rock.position = rock_start_position.global_position
+	new_rock.set_direction(self.direction)
 #
 #func _on_animated_sprite_2d_animation_finished() -> void:
 	#if animation.animation == "attack":
