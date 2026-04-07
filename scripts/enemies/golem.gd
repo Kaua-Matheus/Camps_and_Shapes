0cb6dff625049f71e8aca9586445111c59502dd4
Kauã Matheus <kauamatheussilva06@gmail.com>
2026-04-06 21:58:50 -0300

    feat: Enemies chases player

--- scripts/enemies/golem.gd
+++ scripts/enemies/golem.gd
@@ -1,12 +1,13 @@
 extends CharacterBody2D
 
+var player_ref = null
+
 @onready var animation: AnimatedSprite2D = $AnimatedSprite2D
 @onready var hitbox: Area2D = $HitBox
 
 # Raycast
 @onready var wall_detector: RayCast2D = $WallDetector
 @onready var ground_detector: RayCast2D = $GroundDetector
-@onready var player_detector: RayCast2D = $PlayerDetector
 
 # Bone
 const ROCK = preload("uid://b6tgl8mkre3l2")
@@ -18,48 +19,55 @@ enum GolemState {
 	attack,
 	#dead
 }
-
-const SPEED = 20.0
-const JUMP_VELOCITY = -250.0
+const SPEED = 100
 
 var status: GolemState
 
-var direction = 1
 var can_throw = true
 
-func _ready() -> void:
-	go_to_walk_state()
+#func _ready() -> void:
+	#go_to_walk_state()
 
 func _physics_process(delta: float) -> void:
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
+	if player_ref != null:
+		var distance: Vector2 = player_ref.global_position - global_position
+		var direction: Vector2 = distance.normalized()
+		var distance_length: float = distance.length()
 		
-	match status:
-		GolemState.idle:
-			idle_state(delta)
-		GolemState.walk:
-			walk_state(delta)
+		if distance_length <= 5:
+			velocity = Vector2.ZERO
+		else:
+			velocity = SPEED * direction
+			
+	else:
+		pass
+		
+	#match status:
+		#GolemState.idle:
+			#idle_state(delta)
+		#GolemState.walk:
+			#walk_state(delta)
 		#GolemState.attack:
 			#attack_state(delta)
-		#GolemState.dead:
-			#dead_state(delta)
+		##GolemState.dead:
+			##dead_state(delta)
 
 	move_and_slide()
 	
 	
-func go_to_idle_state():
-	status = GolemState.idle
-	animation.play("idle")
-	
-func go_to_walk_state():
-	status = GolemState.walk
-	animation.play("walk")
-	
-func go_to_attack_state():
-	status = GolemState.attack
-	#animation.play("attack")
-	velocity = Vector2.ZERO
-	can_throw = true
+#func go_to_idle_state():
+	#status = GolemState.idle
+	#animation.play("idle")
+	#
+#func go_to_walk_state():
+	#status = GolemState.walk
+	#animation.play("walk")
+	#
+#func go_to_attack_state():
+	#status = GolemState.attack
+	##animation.play("attack")
+	#velocity = Vector2.ZERO
+	#can_throw = true
 	
 #func go_to_dead_state():
 	#status = GolemState.dead
@@ -68,41 +76,45 @@ func go_to_attack_state():
 	#hitbox.process_mode = Node.PROCESS_MODE_DISABLED
 	
 	
-func idle_state(_delta):
-	pass
+#func idle_state(_delta):
+	#pass
 	
-func walk_state(_delta):
-	velocity.x = SPEED * direction
-	
-	if wall_detector.is_colliding():
-		scale.x *= -1
-		direction *= -1
+#func walk_state(_delta):
+	#velocity.x = SPEED * direction
+	#
+	#if wall_detector.is_colliding():
+		#scale.x *= -1
+		#direction *= -1
 		
 	#if not ground_detector.is_colliding():
 		#scale.x *= -1
 		#direction *= -1
-		
-	if player_detector.is_colliding():
-		go_to_attack_state()
-		return
 	
 #func dead_state(_delta):
 	#pass
 #
-func attack_state(_delta):
-	throw_rock()
-	can_throw = false
+#func attack_state(_delta):
+	#throw_rock()
+	#can_throw = false
 #
 #func take_damage():
 	#go_to_dead_state()
 #
-func throw_rock():
-	var new_rock = ROCK.instantiate()
-	add_sibling(new_rock)
-	new_rock.position = rock_start_position.global_position
-	new_rock.set_direction(self.direction)
+#func throw_rock():
+	#var new_rock = ROCK.instantiate()
+	#add_sibling(new_rock)
+	#new_rock.position = rock_start_position.global_position
+	#new_rock.set_direction(self.direction)
 #
 #func _on_animated_sprite_2d_animation_finished() -> void:
 	#if animation.animation == "attack":
 		#go_to_walk_state()
 		#return
+
+func on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		player_ref = body
+
+func on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		player_ref = null
