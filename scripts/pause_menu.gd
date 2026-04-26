ac87fe20ce5594c136a587997213accb45cc4d14
Kauã Matheus <kauamatheussilva06@gmail.com>
2026-04-26 20:01:23 -0300

    feat: Added pause in game

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -5,6 +5,7 @@ const GAME_SCENE := "res://scenes/maps/level.tscn"
 
 func _ready() -> void:
 	$MenuButtons/BtnContinue.disabled = not FileAccess.file_exists(SAVE_FILE)
+	process_mode = Node.PROCESS_MODE_WHEN_PAUSED
 
 func _on_btn_continue_pressed() -> void:
 	#get_tree().change_scene_to_file(GAME_SCENE)
