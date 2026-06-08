195a3c0bd6725a923c196531252c0cb10ad60be9
Kauã Matheus <kauamatheussilva06@gmail.com>
2026-06-08 19:49:50 -0300

    fix: Golem state machine

--- scripts/character/enemies/golem.gd
+++ scripts/character/enemies/golem.gd
@@ -14,80 +14,79 @@ enum GolemState {
 	#dead
 }
 
+var current_state: GolemState
+
 @export var speed: int = 100
 @export var melee_range: float = 40.0
 
 @export var damage_percent: float = 20.0
+
+var distance: Vector2
+var direction: Vector2
+var distance_length: float
+
 var damage_cooldown: float = 1.5
 var damage_timer: float = 0.0
-
-var status: GolemState
 
 func _ready() -> void:
 	
 	# Hp
 	current_hp = max_hp
 	
-	go_to_idle_state()
+	enter_idle_state()
 
 
 func _physics_process(delta: float) -> void:
 	damage_timer -= delta * damage_cooldown
 
-	if is_instance_valid(player_ref):
-		go_to_walk_state()
-		var distance: Vector2 = player_ref.global_position - global_position
-		var direction: Vector2 = distance.normalized()
-		var distance_length: float = distance.length()
-
-		if distance_length <= melee_range:
-			velocity = Vector2.ZERO
-			if damage_timer <= 0.0:
-				player_ref.take_attack_damage_percent(damage_percent)
-				damage_timer = damage_cooldown
-		else:
-			velocity = speed * direction
-
-	else:
-		velocity = Vector2.ZERO
-		go_to_idle_state()
-
-	#match status:
-		#GolemState.idle:
-			#idle_state(delta)
-		#GolemState.walk:
-			#walk_state(delta)
-		#GolemState.attack:
-			#attack_state(delta)
-		##GolemState.dead:
-			##dead_state(delta)
+	update_state(delta)
 
 	move_and_slide()
 	
 	
-func go_to_idle_state():
-	status = GolemState.idle
+func enter_idle_state():
+	current_state = GolemState.idle
 	animation.play("idle")
 	
-func go_to_walk_state():
-	status = GolemState.walk
+func enter_walk_state():
+	current_state = GolemState.walk
 	animation.play("walk")
 	
-#func go_to_attack_state():
-	#status = GolemState.attack
+#func enter_attack_state():
+	#current_state = GolemState.attack
 	##animation.play("attack")
 	#velocity = Vector2.ZERO
 	#can_throw = true
 	
-#func go_to_dead_state():
-	#status = GolemState.dead
+#func enter_dead_state():
+	#current_state = GolemState.dead
 	#animation.play("dead")
 	#velocity = Vector2.ZERO
 	#hitbox.process_mode = Node.PROCESS_MODE_DISABLED
 	
 	
 func idle_state(_delta):
-	pass
+	if is_instance_valid(player_ref):
+		enter_walk_state()
+		return
+	else:
+		velocity = Vector2.ZERO
+		enter_idle_state()
+		return
+	
+func walk_state(_delta):
+	distance = player_ref.global_position - global_position
+	direction = distance.normalized()
+	distance_length = distance.length()
+
+	if distance_length <= melee_range:
+		velocity = Vector2.ZERO
+		if damage_timer <= 0.0:
+			player_ref.take_attack_damage_percent(damage_percent)
+			damage_timer = damage_cooldown
+	
+	else:
+		velocity = speed * direction
 	
 		
 func die() -> void:
@@ -104,6 +103,17 @@ func take_damage(amount: int) -> void:
 
 func take_attack_damage_percent(percent: float) -> void:
 	take_damage(int(max_hp * percent / 20.0))
+
+
+func update_state(delta: float):
+	
+	match current_state:
+
+		GolemState.idle:
+			idle_state(delta)
+
+		GolemState.walk:
+			walk_state(delta)
 
 
 func player_body_entered(body: Node2D) -> void:
