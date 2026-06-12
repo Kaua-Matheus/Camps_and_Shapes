0802b7e6adc1fc67b33325f4df038ddc5a3060de
Gabriel Felipe Kuhn Schiestl <gfkschiestl@minha.fag.edu.br>
2026-06-12 12:09:15 -0300

    fix: adjust campfire sprite

--- scripts/other/campfire.gd
+++ scripts/other/campfire.gd
@@ -1,35 +1,23 @@
 class_name Campfire
 extends Node2D
 
-const CAMPFIRE_SHEET := preload("res://exterior tilesets/Campfire_Sheet.png")
-const FRAME_COUNT := 7
 const FPS := 8.0
+const FRAME_COUNT := 5
 
-@onready var anim_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var sprite: Sprite2D = $Sprite2D
 @onready var warm_zone: Area2D = $WarmZone
 
+var _frame_timer: float = 0.0
+
 func _ready() -> void:
-	_setup_animation()
 	warm_zone.body_entered.connect(_on_body_entered)
 	warm_zone.body_exited.connect(_on_body_exited)
-	anim_sprite.play("burn")
 
-func _setup_animation() -> void:
-	var frames := SpriteFrames.new()
-	frames.add_animation("burn")
-	frames.set_animation_loop("burn", true)
-	frames.set_animation_speed("burn", FPS)
-
-	var frame_w: int = CAMPFIRE_SHEET.get_width() / FRAME_COUNT
-	var frame_h: int = CAMPFIRE_SHEET.get_height()
-
-	for i in FRAME_COUNT:
-		var atlas := AtlasTexture.new()
-		atlas.atlas = CAMPFIRE_SHEET
-		atlas.region = Rect2(i * frame_w, 0, frame_w, frame_h)
-		frames.add_frame("burn", atlas)
-
-	anim_sprite.sprite_frames = frames
+func _process(delta: float) -> void:
+	_frame_timer += delta
+	if _frame_timer >= 1.0 / FPS:
+		_frame_timer -= 1.0 / FPS
+		sprite.frame = (sprite.frame + 1) % FRAME_COUNT
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
